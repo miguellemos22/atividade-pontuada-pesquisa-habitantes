480f225212b66239178822ffab8d5d554a60bc04
AmandaSantos <contatoflorencecity@gmail.com>
2024-10-03 19:42:23 +0000

    Salvando dupla

--- Atividade 01 (colaborador).py
+++ Atividade 01 (colaborador).py
@@ -1,3 +1,9 @@
+print("""
+    Integrantes:
+    Amanda Almeida Lima dos Santos
+    Miguel Lemos de Oliveira Esquivel
+""")
+
 import os
 from dataclasses import dataclass
 os.system("cls || clear")
@@ -6,7 +12,7 @@ os.system("cls || clear")
 class Dados:
     salario: float
     idade: int
-    sexo: int
+    sexo: str
 
 lista_pesquisa = []
 
@@ -22,35 +28,36 @@ while True:
 
     match codigo:
         case 1:
-            continuar = input("Deseja adicionar uma família? (sim/não): ")
-            while continuar.lower() == 'sim':
                 salario = float(input("Digite o seu salário: "))
-                continuar = input("Deseja adicionar outra família? (sim/não): ")
-                idade = int(input("Digite a quantidade de filhos que você tem: "))
-                sexo = int(input("Digite o seu sexo(M/F): "))
+                idade = int(input("Digite a sua idade: "))
+                sexo = str(input("Digite o seu sexo(M/F): ")).upper()
                 lista_pesquisa.append(Dados(salario=salario, idade=idade, sexo=sexo))
+                
+                continuar = input("Deseja adicionar outra familia? (sim/não): ")
+                while continuar.lower() != 'sim':
+                    break
         case 2:
             nome_do_arquivo = "Pesquisa_prefeitura.txt"
-            with open(nome_do_arquivo, "a") as arquivo_alunos:
+            with open(nome_do_arquivo, "w") as arquivo_alunos:
                 for dado in lista_pesquisa:
                     arquivo_alunos.write(f"{dado.salario}, {dado.idade}, {dado.sexo}\n")
 
             salarios = [dado.salario for dado in lista_pesquisa]
-            idade_total = sum(dado.idade for dado in lista_pesquisa)
+            idade = [dado.idade for dado in lista_pesquisa]
 
             contador = len(lista_pesquisa)
             soma = sum(salarios)
             media = soma / contador
             maior = max(idade)
             menor = min(idade)
+            total_mulheres = sum(1 for dado in lista_pesquisa if dado.sexo == "F" and dado.salario == 5000)
 
             print(" === RESULTADOS === ")
             print(f"Total de famílias que responderam: {contador}")
             print(f"Salário maior: {maior}, salário menor: {menor}")
             print(f"Soma do salário: {soma}")
             print(f"Média do salário: {media}")
-            print(f"Nº total de filhos: {idade_total}")
-
+            print(f"Quantidade de mulheres com salário de R$5.000: {total_mulheres}")
         case 3:
             print("Saindo do sistema")
             break
