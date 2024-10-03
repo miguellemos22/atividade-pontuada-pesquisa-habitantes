56a38cdcfb6e5ab5b65d1db2674ca37532e61057
AmandaSantos <contatoflorencecity@gmail.com>
2024-10-03 17:44:39 +0000

    Salvando pesquisa

--- Atividade 01 (colaborador).py
+++ Atividade 01 (colaborador).py
@@ -5,14 +5,15 @@ os.system("cls || clear")
 @dataclass
 class Dados:
     salario: float
-    filhos: int
+    idade: int
+    sexo: int
 
 lista_pesquisa = []
 
 # === MENU ===
 while True:
     print("""
-        Código {1} = Adicionar família
+        Código {1} = Adicionar pessoa
         Código {2} = Exibir resultados
         Código {3} = Sair
           """)
@@ -25,29 +26,30 @@ while True:
             while continuar.lower() == 'sim':
                 salario = float(input("Digite o seu salário: "))
                 continuar = input("Deseja adicionar outra família? (sim/não): ")
-                filhos = int(input("Digite a quantidade de filhos que você tem: "))
-                lista_pesquisa.append(Dados(salario=salario, filhos=filhos))
+                idade = int(input("Digite a quantidade de filhos que você tem: "))
+                sexo = int(input("Digite o seu sexo(M/F): "))
+                lista_pesquisa.append(Dados(salario=salario, idade=idade, sexo=sexo))
         case 2:
             nome_do_arquivo = "Pesquisa_prefeitura.txt"
             with open(nome_do_arquivo, "a") as arquivo_alunos:
                 for dado in lista_pesquisa:
-                    arquivo_alunos.write(f"{dado.salario}, {dado.filhos}\n")
+                    arquivo_alunos.write(f"{dado.salario}, {dado.idade}, {dado.sexo}\n")
 
             salarios = [dado.salario for dado in lista_pesquisa]
-            filhos_total = sum(dado.filhos for dado in lista_pesquisa)
+            idade_total = sum(dado.idade for dado in lista_pesquisa)
 
             contador = len(lista_pesquisa)
             soma = sum(salarios)
             media = soma / contador
-            maior = max(salarios)
-            menor = min(salarios)
+            maior = max(idade)
+            menor = min(idade)
 
             print(" === RESULTADOS === ")
             print(f"Total de famílias que responderam: {contador}")
             print(f"Salário maior: {maior}, salário menor: {menor}")
             print(f"Soma do salário: {soma}")
             print(f"Média do salário: {media}")
-            print(f"Nº total de filhos: {filhos_total}")
+            print(f"Nº total de filhos: {idade_total}")
 
         case 3:
             print("Saindo do sistema")
